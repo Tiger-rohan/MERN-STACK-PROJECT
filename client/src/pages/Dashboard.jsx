1e5db4f16d8e577bea88091ce9b65a98008094c5
Tiger-rohan <rohan.kumar02@tigeranalytics.com>
2024-06-01 16:20:08 +0530

    added css

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,14 +1,15 @@
 import { useContext } from 'react'
 import { UserContext } from '../../context/userContext';
-
+import './pages.css'; // Import the CSS file
 
 export default function Dashboard() {
 
-const {user} = useContext(UserContext)
+  const {user} = useContext(UserContext);
+  
   return (
-    <div>
-      <h1>Dashboard</h1>
-      {!!user && (<h1>Hi {user.name}!</h1>)}
+    <div className="dashboard-container">
+      <h1 className="dashboard-heading">Dashboard</h1>
+      {!!user && (<h1 className="user-greeting">Hi {user.name}!</h1>)}
     </div>
   )
 }
