94de638dda940371d715c9def1b88590ad57c694
J Naveen Raaj <naveen.raaj@tigeranalytics.com>
2024-07-15 16:11:53 +0530

    Created new branch 1. updated package.json to start both the servers concurrently.

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name:String,
-    email: {
-    type:String,
-    unique:true
-},
-    password:String
-})
+  name: {
+    type: String,
+    required: true 
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true 
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
 
-const UserModel =mongoose.model('User',userSchema)
+const UserModel = mongoose.model('User', userSchema);
 
 module.exports = UserModel;
