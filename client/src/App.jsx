c9e4e85663dbb07591511874630f2bf61a2c0429
Tiger-rohan <rohan.kumar02@tigeranalytics.com>
2024-07-17 16:02:05 +0530

    mui added to the ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 // import Navbar from '../src/components/Navbar'
-import Home from './pages/Home'
+
 import Login from './pages/Login'
 import Register from './pages/Register'
 import axios from 'axios'
 import {Toaster} from 'react-hot-toast'
 import {UserContextProvider} from '../context/userContext'
-import Dashboard from './pages/Dashboard'
+
 import Admin from './components/Admin/Admin'
 import User from './components/User/User'
 
@@ -22,8 +22,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/admin' element={<Admin />} />
         <Route path='/user' element={<User />} />
       </Routes>
