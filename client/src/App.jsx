1ab149c7ee9c7b1d36bd159dc22905fe5234573b
J Naveen Raaj <naveen.raaj@tigeranalytics.com>
2024-07-17 14:09:10 +0530

    Updated code to push the data to "/userDetails" api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
-import Navbar from '../src/components/Navbar'
+// import Navbar from '../src/components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
@@ -20,9 +20,8 @@ function App() {
       
       <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} />
       <Routes>
-        <Route path='/' element={<Navigate to='/login' />} />
+        <Route path='/' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
         <Route path='/home' element={<Home />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/admin' element={<Admin />} />
