13a0f1df4c3c9f765e5123918910019b52744d92
Tiger-rohan <rohan.kumar02@tigeranalytics.com>
2024-07-17 12:26:07 +0530

    put a logout button and fixed admin page

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import ProjectCreation from "./components/ProjectCreation";
 import CreateTask from "./components/Tasks/CreateTasks";
+import './Admin.css';
 
 function Admin() {
-    return <div>
-        <ProjectCreation />
-        <CreateTask/>
-    </div>
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        // Clear authorization token (example for cookies)
+        document.cookie = 'authToken=; Max-Age=-1;'; // Set Max-Age to expire cookie immediately
+
+        // Redirect to login page
+        navigate('/login');
+    };
+
+    return (
+        <>
+            <h1>Admin</h1>
+
+            <div className="admin-container">
+                <div className="form-container">
+                    <ProjectCreation />
+                </div>
+                <div className="form-container">
+                    <CreateTask />
+                </div>
+            </div>
+
+            {/* Logout Button */}
+            <button className="logout-button" onClick={handleLogout}>Logout</button>
+        </>
+    );
 };
 
 export default Admin;
--- client/src/components/Admin/components/Tasks/CreateTasks.jsx
+++ client/src/components/Admin/components/Tasks/CreateTasks.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+
 const CreateTask = () => {
   const [taskDescription, setTaskDescription] = useState('');
   const [taskDueDate, setTaskDueDate] = useState('');
