c88ec77a9433ba9dc784b0413f628f7532e557b5
Tiger-rohan <rohan.kumar02@tigeranalytics.com>
2024-07-18 19:16:02 +0530

    Bg color fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,34 @@
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
-// import Navbar from '../src/components/Navbar'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+// import Navbar from '../src/components/Navbar';
 
-import Login from './pages/Login'
-import Register from './pages/Register'
-import axios from 'axios'
-import {Toaster} from 'react-hot-toast'
-import {UserContextProvider} from '../context/userContext'
+import Login from './pages/Login';
+import Register from './pages/Register';
+import axios from 'axios';
+import { Toaster } from 'react-hot-toast';
+import { UserContextProvider } from '../context/userContext';
+import { useEffect } from 'react';
 
-import Admin from './components/Admin/Admin'
-import User from './components/User/User'
+import Admin from './components/Admin/Admin';
+import User from './components/User/User';
 
 axios.defaults.baseURL = 'http://localhost:8000';
 axios.defaults.withCredentials = true;
 
 function App() {
+  useEffect(() => {
+    document.body.style.backgroundColor = '#28282B';
+    document.body.style.color = 'white'; // Optional: to make text readable
+
+    // Cleanup function to reset the background color when the component unmounts
+    return () => {
+      document.body.style.backgroundColor = '';
+      document.body.style.color = '';
+    };
+  }, []);
+
   return (
     <UserContextProvider>
-      
       <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} />
       <Routes>
         <Route path='/' element={<Login />} />
