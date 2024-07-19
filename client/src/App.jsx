22bc24245592410ac9805d0337aa77e80d685943
J Naveen Raaj <naveen.raaj@tigeranalytics.com>
2024-07-19 10:38:49 +0530

    Rebased with 'rohan' branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import {thunk} from 'redux-thunk'; // Remove the curly braces
 import { Toaster } from 'react-hot-toast';
-import axios from 'axios';
 
 import rootReducer from './reducers/rootReducers'; // Adjust the path as necessary
 import Home from './pages/Home';
@@ -16,21 +15,12 @@ import User from './components/User/User';
 import ProjectCreation from './components/Admin/components/ProjectCreation'; // Adjust the path as necessary
 import CreateTask from './components/Admin/components/Tasks/CreateTasks' // Ensure to import CreateTask
 
-import { UserContextProvider } from '../context/userContext'; // Adjust the path as necessary
-
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
 // import Navbar from '../src/components/Navbar';
 
-import Login from './pages/Login';
-import Register from './pages/Register';
 import axios from 'axios';
-import { Toaster } from 'react-hot-toast';
 import { UserContextProvider } from '../context/userContext';
 import { useEffect } from 'react';
 
-import Admin from './components/Admin/Admin';
-import User from './components/User/User';
 
 
 axios.defaults.baseURL = 'http://localhost:8000';
@@ -51,7 +41,7 @@ function App() {
   }, []);
 
   return (
-    <Provider store={store}>
+    <><Provider store={store}>
       <UserContextProvider>
         <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} />
         <Routes>
@@ -65,17 +55,15 @@ function App() {
           <Route path='/create-task' element={<CreateTask />} /> {/* Add CreateTask route */}
         </Routes>
       </UserContextProvider>
-    </Provider>
-
-    <UserContextProvider>
-      <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} />
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/admin' element={<Admin />} />
-        <Route path='/user' element={<User />} />
-      </Routes>
-    </UserContextProvider>
+    </Provider><UserContextProvider>
+        <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} />
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/admin' element={<Admin />} />
+          <Route path='/user' element={<User />} />
+        </Routes>
+      </UserContextProvider></>
 
   );
 }
